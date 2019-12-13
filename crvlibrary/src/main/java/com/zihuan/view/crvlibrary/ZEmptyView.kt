4a190ef6c194d896f73b72c6f52003afe571221f
紫幻 <zihuancz@foxmail.com>
2019-12-13 12:30:03 +0800

    fixbug

--- crvlibrary/src/main/java/com/zihuan/view/crvlibrary/ZEmptyView.kt
+++ crvlibrary/src/main/java/com/zihuan/view/crvlibrary/ZEmptyView.kt
@@ -2,12 +2,15 @@ package com.zihuan.view.crvlibrary
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.FrameLayout
 
 
 class ZEmptyView : FrameLayout {
     constructor(context: Context) : super(context) {
         visibility = View.GONE
+        layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
         inflate(context, emptyLayout, this)
     }
 
